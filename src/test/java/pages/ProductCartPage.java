aa87a3576c33d0f22ee0779e274bb69e57599620
nmoulika22 <moulika.kolusu@gmail.com>
2021-11-07 14:59:53 +0000

    Updated cart icon property

--- src/test/java/pages/ProductCartPage.java
+++ src/test/java/pages/ProductCartPage.java
@@ -19,16 +19,15 @@ public class ProductCartPage {
 		Thread.sleep(10000);
 		int expectedQuantityInCart = 1;
 		WebElement cartItemCount = driver.findElement(By.cssSelector("div.badge-count"));
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", cartItemCount);
 		Thread.sleep(1000);
 		Assert.assertEquals(expectedQuantityInCart, Integer.parseInt(cartItemCount.getText()));
-		WebElement cartButton = driver.findElement(By.cssSelector("span[class='badge-icon cart-icon']"));
+		WebElement cartButton = driver.findElement(By.cssSelector("span.cart-icon"));
 		JavascriptExecutor executor = (JavascriptExecutor)driver;
 		executor.executeScript("arguments[0].click();", cartButton);
-		Thread.sleep(10000);
+		Thread.sleep(20000);
 		System.out.println(driver.getTitle());
 		WebElement productInCartPage = driver.findElement(By.cssSelector("a h3"));
-		System.out.println(productInCartPage.getText());
+		System.out.println(productInCartPage.getText().trim());
 		Assert.assertEquals(expectedProductInCart, productInCartPage.getText());
 	}
 }
